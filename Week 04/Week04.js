31cf48cd9f58ea25b543fbc8f68246c51b4ec20f
SehanUdayakantha <102642326+SehanUdayakantha@users.noreply.github.com>
2022-09-09 16:02:37 +0930

    Add files via upload

---
+++ Week 04/Week04.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        text: 'Hello World!',
+        show: true,
+        seen: false,
+        datalist:[
+            {text: 'display'},
+            {text: 'a'},
+            {text: 'list'}
+        ]
+    }
+});
